4d62b2c1d3fc701223051b0b343d4d1b8c60717e
Anthony Benitez <anthony.bebor@gmail.com>
2021-08-29 19:01:53 -0400

    Replace all echo executions with printf

--- pacman-system-upgrade.sh
+++ pacman-system-upgrade.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # confirm root is executing
-[[ $UID -eq 0 ]] || { echo 'script requires elevated privileges!' ; exit 1 ; }
+[[ $UID -eq 0 ]] || { printf 'script requires elevated privileges!\n' ; exit 1 ; }
 
 # confirm system is arch linux
-[[ -f /etc/arch-release ]] || { echo 'system is not arch linux!' ; exit 1 ; }
+[[ -f /etc/arch-release ]] || { printf 'system is not arch linux!\n' ; exit 1 ; }
 
 # log since last system upgrade
 tmp_log="$(tac /var/log/pacman.log | sed -n '0,/starting full system upgrade/p' | tac)"
@@ -18,14 +18,14 @@ d2e () { date -d "$(cut -d ' ' -f 1 <<< $1) 00:00" +%s ; }
 # extract news by filtering html from curl
 empty=''
 while read -d $'\n' line ; do
-    [[ $(d2e "$line") -gt $last_upgrade ]] && { echo "$line" ; empty='nope' ; }
+    [[ $(d2e "$line") -gt $last_upgrade ]] && { printf "$line\n" ; empty='nope' ; }
 done < <(curl -s https://archlinux.org/news/ \
         | awk '/<td>[0-9\-]{10}<\/td>|title="View:/' \
         | sed -E 's|^ +||;s|^<td>([0-9\-]{10})</td>|\1|;s|^title=.+>(.+)</a></td>$|\1|' \
         | sed '$!N;s|\n| |'
 )
 
-[[ -z $empty ]] && echo -e 'No Arch Linux news since last upgrade!\n'
+[[ -z $empty ]] && printf 'No Arch Linux news since last upgrade!\n'
 
 # execute pacman sync and upgrade
 pacman -Syu
